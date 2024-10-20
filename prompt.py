8ec6a1af0b80fe56a872e57739143aa4dba60d57
toyhammer98 <82231608+toyhammer98@users.noreply.github.com>
2024-10-20 17:28:17 -0400

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,13 @@
+def report_count(target):
+  count = 0
+  with open("corpus.txt", "r") as n:
+    for line in n:
+      #cleaning the text
+      line = line.strip()
+      line = line.lower()
+      words = line.split(" ")
+      #counting occurrences of target word
+      for i in words:
+        if (i == target):
+          count += 1
+  print("The term ",target," shows up in the corpus ",count," times.")
